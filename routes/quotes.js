ba359c76026c1d358f5acf8ffe8c71b965e6f8b8
orpichardo <orlandopichardo809@gmail.com>
2022-12-20 20:26:51 -0500

    Cannot upload to database, changed module to modules in .gitignore did not work

--- routes/quotes.js
+++ routes/quotes.js
@@ -4,7 +4,7 @@ import * as controllers from "../controllers/quotes.js";
 const router = Router();
 
 router.get("/", controllers.getQuotes);
-router.get("/:id", controllers.getQuotes1);
+router.get("/:id", controllers.getQuotes);
 router.post("/", controllers.createQuotes);
 router.put("/:id", controllers.updateQuotes);
 router.delete("/:id", controllers.deleteQuotes);
